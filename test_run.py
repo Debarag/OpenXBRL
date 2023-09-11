84af33a195b30e3a67292ab33becb248808d70ef
Efraim <>
2023-09-11 08:25:08 -0400

    Split Downloader() function get() into query part (get_urls()) and download (get_files()).

--- test_run.py
+++ test_run.py
@@ -4,14 +4,18 @@ Sample script to test and run downloaders and parsers.
 
 from openxbrl import Downloader, AccountingParser
 
-test_download = False
-test_parse    = True
+test_download = True
+test_parse    = False
 
 if( test_download ) :
-  downloader = Downloader( './sec-data/forms' )
-  ciks = [1750] # AAR Corp # [1341439, 34088]  # Oracle Corp, ExxonMobil
-  forms = ["10-K", "10-K/A", "10-Q", "10-Q/A"]
-  downloader.get(ciks, forms, '2000-01-01', '2023-06-30')
+  downloader = Downloader(  )
+  ciks      = [1750] # AAR Corp # [1341439, 34088]  # Oracle Corp, ExxonMobil
+  forms     = ["10-K", "10-K/A", "10-Q", "10-Q/A"]
+  from_date = '2022-01-01'
+  to_date   = '2023-09-01'
+  df = downloader.get_urls(ciks, forms, from_date, to_date)
+  print(df)
+  downloader.get_files(ciks, forms, from_date, to_date, workingdir='./sec-data/forms')
 
 if( test_parse ) :
     # Assumes companyfacts.zip from SEC has been downloaded and unzipped
